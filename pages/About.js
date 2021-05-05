12a3c07e5e38bb6878365b8a964ef325da1ae23b
krishnadevz <krishnakakade77@gmail.com>
2021-05-05 13:01:41 +0530

    auto routing tried

---
+++ pages/About.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+import React from 'react'
+
+export default function About() {
+    return (
+      <div>
+          <Head>
+              <title>About</title>
+          </Head>
+        <h1> About</h1>
+        
+      </div>
+    )
+  }
+  
